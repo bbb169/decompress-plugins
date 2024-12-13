c2c62f632e604be8325679fe1c21d104a45751c6
jingzouzou <827088092@qq.com>
2024-12-13 21:51:26 +0800

    feat: add demo codes

--- packages/plugin-7z/src/index.ts
+++ packages/plugin-7z/src/index.ts
@@ -2,4 +2,58 @@
 export const pluginZip = async (input: string, output: string): Promise<void> => {
   console.log('7z');
   
-};
+const handle7zFile = () => (input: Buffer | Stream) => {
+	if (!Buffer.isBuffer(input) && !isStream(input)) {
+		return Promise.reject(new TypeError(`Expected a Buffer or Stream, got ${typeof input}`));
+	}
+
+	if (Buffer.isBuffer(input) && (!fileTypeFromBuffer(input) || fileTypeFromBuffer(input).ext !== 'tar')) {
+    
+    return Promise.resolve([]);
+	} 
+
+	const extract = tarStream.extract();
+	const files = [];
+
+	extract.on('entry', (header, stream, cb) => {
+		const chunk = [];
+
+		stream.on('data', data => chunk.push(data));
+		stream.on('end', () => {
+			const file = {
+				data: Buffer.concat(chunk),
+				mode: header.mode,
+				mtime: header.mtime,
+				path: header.name,
+				type: header.type
+			};
+
+			if (header.type === 'symlink' || header.type === 'link') {
+				file.linkname = header.linkname;
+			}
+
+			files.push(file);
+			cb();
+		});
+	});
+
+	const promise = new Promise((resolve, reject) => {
+		if (!Buffer.isBuffer(input)) {
+			input.on('error', reject);
+		}
+
+		extract.on('finish', () => resolve(files));
+		extract.on('error', reject);
+	});
+
+	extract.then = promise.then.bind(promise);
+	extract.catch = promise.catch.bind(promise);
+
+	if (Buffer.isBuffer(input)) {
+		extract.end(input);
+	} else {
+		input.pipe(extract);
+	}
+
+	return extract;
+}

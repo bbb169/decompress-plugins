e797b2da9bcbd75960841c6fdea2d929f80e4d8e
jingzouzou <827088092@qq.com>
2024-12-15 23:16:48 +0800

    feat: trying use rar-stream to read file

--- packages/plugin-rar/src/index.ts
+++ packages/plugin-rar/src/index.ts
@@ -1,6 +1,64 @@
+'use strict';
 
+import { Stream } from "stream";
+import { fileTypeFromBuffer, fileTypeStream } from 'file-type';
+import { isStream } from 'is-stream';
+import { LocalFileMedia, RarFilesPackage } from 'rar-stream';
 
-export const pluginZip = async (input: string, output: string): Promise<void> => {
-  console.log('xix');
-  
-};
+const handleRarFile = () => (input: Buffer | Stream) => {
+	if (!Buffer.isBuffer(input) && !isStream(input)) {
+		return Promise.reject(new TypeError(`Expected a Buffer or Stream, got ${typeof input}`));
+	}
+
+	if (Buffer.isBuffer(input) && (!fileTypeFromBuffer(input) || fileTypeFromBuffer(input).ext !== 'tar')) {
+    
+    return Promise.resolve([]);
+	} 
+  const abc = new Stream()
+  abc.resu
+  new LocalFileMedia()
+	const extract = new RarFilesPackage(input);
+	const files = [];
+
+	extract.on('entry', (header, stream, cb) => {
+		const chunk = [];
+
+		stream.on('data', data => chunk.push(data));
+		stream.on('end', () => {
+			const file = {
+				data: Buffer.concat(chunk),
+				mode: header.mode,
+				mtime: header.mtime,
+				path: header.name,
+				type: header.type
+			};
+
+			if (header.type === 'symlink' || header.type === 'link') {
+				file.linkname = header.linkname;
+			}
+
+			files.push(file);
+			cb();
+		});
+	});
+
+	const promise = new Promise((resolve, reject) => {
+		if (!Buffer.isBuffer(input)) {
+			input.on('error', reject);
+		}
+
+		extract.on('finish', () => resolve(files));
+		extract.on('error', reject);
+	});
+
+	extract.then = promise.then.bind(promise);
+	extract.catch = promise.catch.bind(promise);
+
+	if (Buffer.isBuffer(input)) {
+		extract.end(input);
+	} else {
+		input.pipe(extract);
+	}
+
+	return extract;
+}
